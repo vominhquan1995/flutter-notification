37716b5652cad71d938b0d1dfeb747cf0230f9c5
Vo <wingofbluesky@yahoo.com>
2019-01-10 13:56:40 +0700

    them badge

--- lib/common/Notification.dart
+++ lib/common/Notification.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_badger/flutter_app_badger.dart';
 import 'dart:async';
 import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
@@ -8,6 +9,9 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:notification_example/pages/DetailNotificationPage.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 class Notification {
   final int id;
@@ -19,13 +23,61 @@ class Notification {
       {this.id, this.title, this.description, this.icon, this.body});
 }
 
+Database database;
+List badge;
+
 const notification = Notification(
-      id: 1,
-      title:
-          'HUTECH thông báo tuyển sinh đào tạo trình độ Thạc sĩ năm 2019 – Đợt 1',
-      description:
-          'Trường Đại học Công nghệ TP.Hồ Chí Minh (HUTECH) thông báo tuyển sinh đào tạo trình độ Thạc sĩ đợt 1 - năm 2019 với 11 chuyên ngành gồm: Kỹ thuật điện, Kỹ thuật cơ điện tử, Kỹ thuật môi trường, Kỹ thuật xây dựng, Công nghệ sinh học, Công nghệ thông tin, Công nghệ thực phẩm, Kế toán, Quản trị kinh doanh, Quản trị Dịch vụ du lịch và lữ hành, tiếng Anh.',
-      body: '<h2>152 người Việt “biến mất” tại Đài Loan đã dấy lên rất nhiều những quan ngại sâu sắc về việc lợi dụng chính sách để phục vụ mục đích cá nhân gây ảnh hưởng đến uy tín, hình ảnh của Việt Nam.</h2> <p>Ngày 26/12, Focus Taiwan dẫn lời cục Du lịch Đài Loan cho biết, 409 trong tổng số 566 du khách nước ngoài đến Đài Loan theo chương trình nới lỏng thủ tục xin thị thực và biến mất là du khách người Việt.</p> <p>Vì đâu mà những du khách biến mất ở Đài Loan kia lại tự biến mình thành người Việt xấu xí như vậy?</p> <figure><img alt="Bạn đọc viết - Chân dung 152 người Việt xấu xí " src="https://media1.nguoiduatin.vn/media/duong-thu/2018/12/27/nguoi-viet-bien-mat-o-dai-loan.jpg" width="450" height="269"> <figcaption> <h3>Hình ảnh camera tại khách sạn ghi cảnh được cho là những người Việt "biến mất" ở Đài Loan.</h3> </figcaption> </figure> <p>Theo tìm hiểu, việc đi xuất khẩu lao động ở Đài Loan hiện nay có mức chi phí thuộc loại thấp nhất trong các nước. Với thời hạn hợp đồng 3 năm, mức chi phí đi Đài Loan chỉ từ 40 triệu đồng đến 120 triệu đồng. Đây là thông tin tham khảo từ một số địa chỉ tư vấn xuất khẩu lao động uy tín.</p> <p>Thêm nữa, các thủ tục đi Đài Loan cũng không quá phức tạp theo con đường chính thống này.</p> <p>Thế nhưng, 152 người đã bất chấp để đi theo con đường du lịch và mang về cho đất nước không phải là sự giàu mạnh, phát triển, là kinh nghiệm hay bất cứ một sự tiên tiến nào mà là bước thụt lùi về nhân cách kèm theo những rắc rối khó có thể gỡ trong ngày một ngày hai với các cơ quan quản lý Nhà nước.</p> <p>Rồi đây, họ sẽ còn phải đối mặt với những xử lý vi phạm trong việc xuất nhập cảnh, không loại trừ vi phạm cả về luật pháp. Như vậy thử hỏi có đáng không?</p> <p><em>*Bài viết thể hiện quan điểm riêng của tác giả.</em></p>');
+    id: 1,
+    title:
+        'HUTECH thông báo tuyển sinh đào tạo trình độ Thạc sĩ năm 2019 – Đợt 1',
+    description:
+        'Trường Đại học Công nghệ TP.Hồ Chí Minh (HUTECH) thông báo tuyển sinh đào tạo trình độ Thạc sĩ đợt 1 - năm 2019 với 11 chuyên ngành gồm: Kỹ thuật điện, Kỹ thuật cơ điện tử, Kỹ thuật môi trường, Kỹ thuật xây dựng, Công nghệ sinh học, Công nghệ thông tin, Công nghệ thực phẩm, Kế toán, Quản trị kinh doanh, Quản trị Dịch vụ du lịch và lữ hành, tiếng Anh.',
+    body:
+        '<h2>152 người Việt “biến mất” tại Đài Loan đã dấy lên rất nhiều những quan ngại sâu sắc về việc lợi dụng chính sách để phục vụ mục đích cá nhân gây ảnh hưởng đến uy tín, hình ảnh của Việt Nam.</h2> <p>Ngày 26/12, Focus Taiwan dẫn lời cục Du lịch Đài Loan cho biết, 409 trong tổng số 566 du khách nước ngoài đến Đài Loan theo chương trình nới lỏng thủ tục xin thị thực và biến mất là du khách người Việt.</p> <p>Vì đâu mà những du khách biến mất ở Đài Loan kia lại tự biến mình thành người Việt xấu xí như vậy?</p> <figure><img alt="Bạn đọc viết - Chân dung 152 người Việt xấu xí " src="https://media1.nguoiduatin.vn/media/duong-thu/2018/12/27/nguoi-viet-bien-mat-o-dai-loan.jpg" width="450" height="269"> <figcaption> <h3>Hình ảnh camera tại khách sạn ghi cảnh được cho là những người Việt "biến mất" ở Đài Loan.</h3> </figcaption> </figure> <p>Theo tìm hiểu, việc đi xuất khẩu lao động ở Đài Loan hiện nay có mức chi phí thuộc loại thấp nhất trong các nước. Với thời hạn hợp đồng 3 năm, mức chi phí đi Đài Loan chỉ từ 40 triệu đồng đến 120 triệu đồng. Đây là thông tin tham khảo từ một số địa chỉ tư vấn xuất khẩu lao động uy tín.</p> <p>Thêm nữa, các thủ tục đi Đài Loan cũng không quá phức tạp theo con đường chính thống này.</p> <p>Thế nhưng, 152 người đã bất chấp để đi theo con đường du lịch và mang về cho đất nước không phải là sự giàu mạnh, phát triển, là kinh nghiệm hay bất cứ một sự tiên tiến nào mà là bước thụt lùi về nhân cách kèm theo những rắc rối khó có thể gỡ trong ngày một ngày hai với các cơ quan quản lý Nhà nước.</p> <p>Rồi đây, họ sẽ còn phải đối mặt với những xử lý vi phạm trong việc xuất nhập cảnh, không loại trừ vi phạm cả về luật pháp. Như vậy thử hỏi có đáng không?</p> <p><em>*Bài viết thể hiện quan điểm riêng của tác giả.</em></p>');
+
+Future setupDatabase() async {
+  var databasesPath = await getDatabasesPath();
+  String path = join(databasesPath, 'demo.db');
+  // // Delete the database
+//     await deleteDatabase(path);
+
+  // open the database
+  database = await openDatabase(path, version: 1,
+      onCreate: (Database db, int version) async {
+    // When creating the db, create the table
+    await db.execute(
+        'CREATE TABLE badge_setting (id TEXT PRIMARY KEY, name TEXT, count INTEGER)');
+
+    // Insert some records in a transaction
+    await db.transaction((txn) async {
+      txn.rawInsert(
+          'INSERT INTO badge_setting(id, name, count) VALUES("1", "notification", 0)');
+      txn.rawInsert(
+          'INSERT INTO badge_setting(id, name, count) VALUES("2", "card_travel", 0)');
+      txn.rawInsert(
+          'INSERT INTO badge_setting(id, name, count) VALUES("3", "list", 0)');
+    });
+  });
+
+  List<Map> list =
+      await database.rawQuery('SELECT id,name,count FROM badge_setting');
+  badge = list.toList();
+  return badge;
+}
+
+void increaseBadge(id) {
+  setState(() {
+    database.rawUpdate(
+        'UPDATE badge_setting SET count = count + 1 WHERE id = ?', [id]);
+  });
+}
+
+void removeBadge(id) {
+  setState(() {
+    database.rawUpdate('UPDATE badge_setting SET count = 0 WHERE id = ?', [id]);
+  });
+}
+
 Function firebaseCloudMessaging_Listeners(BuildContext context) {
   FirebaseMessaging firebaseMessaging = new FirebaseMessaging();
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
@@ -59,6 +111,8 @@ Function firebaseCloudMessaging_Listeners(BuildContext context) {
   });
   //config event revice notify on 3 state
   firebaseMessaging.configure(onMessage: (Map<String, dynamic> msg) {
+    FlutterAppBadger.updateBadgeCount(3);
+    increaseBadge(msg["data"]["page_id"]);
     showNotification(flutterLocalNotificationsPlugin, msg);
     //       _showBigPictureNotification();
     //        _showBigTextNotification(msg);
@@ -68,6 +122,8 @@ Function firebaseCloudMessaging_Listeners(BuildContext context) {
         toastLength: Toast.LENGTH_SHORT,
         gravity: ToastGravity.BOTTOM);
   }, onLaunch: (Map<String, dynamic> msg) {
+    FlutterAppBadger.updateBadgeCount(3);
+    increaseBadge(msg["data"]["page_id"]);
     // print(" onLaunch called ${(msg)}");
     Fluttertoast.showToast(
         msg: "onLaunch called",
--- lib/pages/HomePape.dart
+++ lib/pages/HomePape.dart
@@ -1,7 +1,9 @@
 import 'dart:math' as math;
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:notification_example/pages/NotificationPage.dart';
 import 'package:notification_example/common/Notification.dart';
+
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
   @override
@@ -15,6 +17,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     firebaseCloudMessaging_Listeners(context);
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,22 +29,55 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             FlatButton(
-              onPressed: () {
-                Navigator.push(context,MaterialPageRoute(builder: (context) => NotificationPage()));
-              },
-              color: Colors.blue,
-              padding: EdgeInsets.all(10.0),
-              child: Column(
-                // Replace with a Row for horizontal icon + text
-                children: <Widget>[
-                  Icon(Icons.notifications, color: Colors.white),
-                  Text(
-                    "Thông báo",
-                    style: TextStyle(color: Colors.white),
-                  )
-                ],
-              ),
-            ),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => NotificationPage()));
+                },
+                color: Colors.blue,
+                padding: EdgeInsets.all(10.0),
+                child: FutureBuilder(
+                    // Column is also layout widget. It takes a list of children and
+                    // arranges them vertically. By default, it sizes itself to fit its
+                    // children horizontally, and tries to be as tall as its parent.
+                    //
+                    // Invoke "debug painting" (press "p" in the console, choose the
+                    // "Toggle Debug Paint" action from the Flutter Inspector in Android
+                    // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+                    // to see the wireframe for each widget.
+                    //
+                    // Column has various properties to control how it sizes itself and
+                    // how it positions its children. Here we use mainAxisAlignment to
+                    // center the children vertically; the main axis here is the vertical
+                    // axis because Columns are vertical (the cross axis would be
+                    // horizontal).
+                    future: setupDatabase(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return new ListView.builder(
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (context, index) {
+                              return new BadgeIconButton(
+                                  itemCount: snapshot.data[index]
+                                      ["count"], // required
+                                  icon: Icon(Icons.notifications,
+                                      size: 30), // required
+                                  badgeColor: Colors.red, // default: Colors.red
+                                  badgeTextColor:
+                                      Colors.white, // default: Colors.white
+                                  hideZeroCount: true, // default: true
+                                  onPressed: () =>
+                                      removeBadge(snapshot.data[index]["id"]));
+                            });
+                      } else if (snapshot.hasError) {
+                        return new Text("${snapshot.error}");
+                      }
+                      return new Container(
+                        alignment: AlignmentDirectional.center,
+                        child: new CircularProgressIndicator(),
+                      );
+                    })),
           ],
         ),
       ),
